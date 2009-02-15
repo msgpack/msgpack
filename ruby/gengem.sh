f4a6d7faa18eba7fad3e72c8238b0c7463c4294f
frsyuki <frsyuki@5a5092ae-2292-43ba-b2d5-dcab9c1a2731>
2009-02-15 09:09:58 +0000

    lang/c/msgpack: Ruby binding: fix gem warning
    git-svn-id: file:///Users/frsyuki/project/msgpack-git/svn/x@71 5a5092ae-2292-43ba-b2d5-dcab9c1a2731

--- ruby/gengem.sh
+++ ruby/gengem.sh
@@ -6,7 +6,7 @@ cp pack.h           gem/ext/
 cp rbinit.c         gem/ext/
 cp unpack.c         gem/ext/
 cp unpack.h         gem/ext/
-cp ../README        gem/README.txt
+#cp ../README        gem/README.txt
 cp ../msgpack/pack_template.h   gem/msgpack/
 cp ../msgpack/unpack_define.h   gem/msgpack/
 cp ../msgpack/unpack_template.h gem/msgpack/
