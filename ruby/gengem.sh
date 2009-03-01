c398d5d82820aaab89ed7e2e407db742376f5d3f
frsyuki <frsyuki@users.sourceforge.jp>
2009-03-01 13:46:20 +0900

    fix packaging problem

--- ruby/gengem.sh
+++ ruby/gengem.sh
@@ -9,7 +9,8 @@ cp rbinit.c         gem/ext/
 cp unpack.c         gem/ext/
 cp unpack.h         gem/ext/
 cat test_case.rb | sed "s/require ['\"]msgpack['\"]/require File.dirname(__FILE__) + '\/test_helper.rb'/" > gem/test/msgpack_test.rb
-#cp ../README        gem/README.txt
+cp ../AUTHORS        gem/AUTHORS
+cp ../ChangeLog      gem/ChangeLog
 cp ../msgpack/pack_define.h     gem/msgpack/
 cp ../msgpack/pack_template.h   gem/msgpack/
 cp ../msgpack/unpack_define.h   gem/msgpack/
