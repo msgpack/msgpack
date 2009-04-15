ea41db4cc7b2e7434b5268233d7e31de07c64517
Tokuhiro Matsuno <tokuhirom@users.sourceforge.jp>
2009-04-15 23:02:27 +0900

    perl: renamed benchmark script

--- perl/benchmark/serialize.pl
+++ perl/benchmark/serialize.pl
@@ -7,6 +7,8 @@ use Benchmark ':all';
 my $a = [0..2**24];
 
 print "-- serialize\n";
+print "JSON::XS: $JSON::XS::VERSION\n";
+print "Data::MessagePack: $Data::MessagePack::VERSION\n";
 cmpthese(
     -1 => {
         json => sub { JSON::XS::encode_json($a)   },
