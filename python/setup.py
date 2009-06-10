a1fb1507d45d27f0c08e8d39054987ed6dcaf9e7
Naoki INADA <inada-n@gear>
2009-06-10 10:45:07 +0900

    Refactor include path.

--- python/setup.py
+++ python/setup.py
@@ -1,8 +1,13 @@
 from distutils.core import setup, Extension
+import os
 
 version = '0.0.1'
 
-msgpack_mod = Extension('msgpack', sources=['msgpack.c'])
+PACKAGE_ROOT = os.getcwdu()
+INCLUDE_PATH = os.path.join(PACKAGE_ROOT, 'include')
+msgpack_mod = Extension('msgpack',
+                        sources=['msgpack.c'],
+                        include_dirs=[INCLUDE_PATH])
 
 desc = 'MessagePack serializer/desirializer.'
 long_desc = desc + """
