3275eee28173e9c4d00641204ab966e7fa278dcd
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-07-30 13:24:38 +0900

    Perl: display original data

--- perl/benchmark/size.pl
+++ perl/benchmark/size.pl
@@ -6,20 +6,24 @@ use Storable;
 use Text::SimpleTable;
 
 my @entries = (
-    1,
-    3.14,
-    {},
-    [],
-    [('a')x10],
-    {('a')x10},
-    +{1,+{1,+{}}},
-    +[+[+[]]],
+    '1',
+    '3.14',
+    '{}',
+    '[]',
+    "[('a')x10]",
+    "{('a')x10}",
+    "+{1,+{1,+{}}}",
+    "+[+[+[]]]",
 );
 
-my $table = Text::SimpleTable->new([10, 'storable'], [10, 'msgpack']);
+my $table = Text::SimpleTable->new([15, 'src'], [9, 'storable'], [7, 'msgpack']);
 
-for my $e (@entries) {
+for my $src (@entries) {
+    my $e = eval $src;
+    die $@ if $@;
+
     $table->row(
+        $src,
         length(Storable::nfreeze(ref $e ? $e : \$e)),
         length(Data::MessagePack->pack($e)),
     );
