ab8e0c9e31585b24f1d4cbfb1e73b0ce249e408a
frsyuki <frsyuki@users.sourceforge.jp>
2010-04-18 00:08:03 +0900

    c,cpp: reforms source tree

---
+++ README.md
@@ -0,0 +1,37 @@
+MessagePack
+===========
+Extremely efficient object serialization library. It's like JSON, but very fast and small.
+
+
+## What's MessagePack?
+
+MessagePack is a binary-based efficient object serialization library. It enables to exchange structured objects between many languages like JSON. But unlike JSON, it is very fast and small.
+
+Typical small integer (like flags or error code) is saved only in 1 byte, and typical short string only needs 1 byte except the length of the string itself. \[1,2,3\] (3 elements array) is serialized in 4 bytes using MessagePack as follows:
+
+    require 'msgpack'
+    msg = [1,2,3].to_msgpack  #=> "\x93\x01\x02\x03"
+    MessagePack.unpack(msg)   #=> [1,2,3]
+
+
+## Performance
+
+![Serialization + Deserialization Speed Test](http://msgpack.sourceforge.net/index/speedtest.png)
+
+In this test, it measured the elapsed time of serializing and deserializing 200,000 target objects. The target object consists of the three integers and 512 bytes string.
+The source code of this test is available from [frsyuki' serializer-speed-test repository.](http://github.com/frsyuki/serializer-speed-test)
+
+
+## Getting Started
+
+Usage and other documents about implementations in each language are found at [the web site.](http://msgpack.sourceforge.net/)
+
+
+## Learn More
+
+  - [Project Web Site](http://msgpack.sourceforge.net/)
+  - [MessagePack format specification](http://msgpack.sourceforge.net/spec)
+  - [Repository at github](http://github.com/msgpack/msgpack)
+  - [Wiki](http://msgpack.sourceforge.net/start)
+  - [MessagePack-RPC](http://github.com/msgpack/msgpack-rpc)
+
---
+++ cpp/msgpack_vc8.postbuild.bat
@@ -0,0 +1,49 @@
+IF NOT EXIST include                  MKDIR include
+IF NOT EXIST include\msgpack          MKDIR include\msgpack
+IF NOT EXIST include\msgpack\type     MKDIR include\msgpack\type
+IF NOT EXIST include\msgpack\type\tr1 MKDIR include\msgpack\type\tr1
+IF EXIST bootstrap (
+	copy ..\msgpack\pack_define.h      include\msgpack\
+	copy ..\msgpack\pack_template.h    include\msgpack\
+	copy ..\msgpack\unpack_define.h    include\msgpack\
+	copy ..\msgpack\unpack_template.h  include\msgpack\
+	copy ..\msgpack\sysdep.h           include\msgpack\
+) ELSE (
+	copy msgpack\pack_define.h      include\msgpack\
+	copy msgpack\pack_template.h    include\msgpack\
+	copy msgpack\unpack_define.h    include\msgpack\
+	copy msgpack\unpack_template.h  include\msgpack\
+	copy msgpack\sysdep.h           include\msgpack\
+)
+copy msgpack.h                     include\
+copy msgpack\sbuffer.h             include\msgpack\
+copy msgpack\vrefbuffer.h          include\msgpack\
+copy msgpack\pack.h                include\msgpack\
+copy msgpack\unpack.h              include\msgpack\
+copy msgpack\object.h              include\msgpack\
+copy msgpack\zone.h                include\msgpack\
+copy msgpack.hpp                   include\
+copy msgpack\sbuffer.hpp           include\msgpack\
+copy msgpack\vrefbuffer.hpp        include\msgpack\
+copy msgpack\pack.hpp              include\msgpack\
+copy msgpack\unpack.hpp            include\msgpack\
+copy msgpack\object.hpp            include\msgpack\
+copy msgpack\zone.hpp              include\msgpack\
+copy msgpack\type.hpp              include\msgpack\type\
+copy msgpack\type\bool.hpp         include\msgpack\type\
+copy msgpack\type\float.hpp        include\msgpack\type\
+copy msgpack\type\int.hpp          include\msgpack\type\
+copy msgpack\type\list.hpp         include\msgpack\type\
+copy msgpack\type\deque.hpp        include\msgpack\type\
+copy msgpack\type\map.hpp          include\msgpack\type\
+copy msgpack\type\nil.hpp          include\msgpack\type\
+copy msgpack\type\pair.hpp         include\msgpack\type\
+copy msgpack\type\raw.hpp          include\msgpack\type\
+copy msgpack\type\set.hpp          include\msgpack\type\
+copy msgpack\type\string.hpp       include\msgpack\type\
+copy msgpack\type\vector.hpp       include\msgpack\type\
+copy msgpack\type\tuple.hpp        include\msgpack\type\
+copy msgpack\type\define.hpp       include\msgpack\type\
+copy msgpack\type\tr1\unordered_map.hpp  include\msgpack\type\
+copy msgpack\type\tr1\unordered_set.hpp  include\msgpack\type\
+
