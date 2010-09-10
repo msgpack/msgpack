f6f675d1e12b2c5994099df3d1af1513b1d83bf2
tokuhirom <tokuhirom@gmail.com>
2010-09-10 20:27:11 +0900

    updated benchmark script

--- perl/benchmark/deserialize.pl
+++ perl/benchmark/deserialize.pl
@@ -3,18 +3,21 @@ use warnings;
 use Data::MessagePack;
 use JSON::XS;
 use Benchmark ':all';
+use Storable;
 
 my $a = [0..2**24];
 my $j = JSON::XS::encode_json($a);
 my $m = Data::MessagePack->pack($a);
+my $s = Storable::nfreeze($a);
 
 print "-- deserialize\n";
 print "JSON::XS: $JSON::XS::VERSION\n";
 print "Data::MessagePack: $Data::MessagePack::VERSION\n";
-cmpthese(
+timethese(
     -1 => {
-        json => sub { JSON::XS::decode_json($j)     },
-        mp   => sub { Data::MessagePack->unpack($m) },
+        json     => sub { JSON::XS::decode_json($j)     },
+        mp       => sub { Data::MessagePack->unpack($m) },
+        storable => sub { Storable::thaw($s) },
     }
 );
 
--- perl/benchmark/serialize.pl
+++ perl/benchmark/serialize.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use Data::MessagePack;
 use JSON::XS;
+use Storable;
 use Benchmark ':all';
 
 my $a = [0..2**24];
@@ -9,9 +10,11 @@ my $a = [0..2**24];
 print "-- serialize\n";
 print "JSON::XS: $JSON::XS::VERSION\n";
 print "Data::MessagePack: $Data::MessagePack::VERSION\n";
-cmpthese(
+print "Storable: $Storable::VERSION\n";
+timethese(
     -1 => {
         json => sub { JSON::XS::encode_json($a)   },
+        storable => sub { Storable::nfreeze($a)   },
         mp   => sub { Data::MessagePack->pack($a) },
     }
 );
