c2bf2a817410b013aecc30d6a5abc0cad0423dff
gfx <gfuji@cpan.org>
2010-09-18 16:16:51 +0900

    perl: make pp benchmarks available

--- perl/benchmark/serialize.pl
+++ perl/benchmark/serialize.pl
@@ -1,19 +1,19 @@
 use strict;
 use warnings;
 use Data::MessagePack;
-use JSON::XS;
+use JSON;
 use Storable;
 use Benchmark ':all';
 
 my $a = do 'benchmark/data.pl';
 
 print "-- serialize\n";
-print "JSON::XS: $JSON::XS::VERSION\n";
+print "$JSON::Backend: ", $JSON::Backend->VERSION, "\n";
 print "Data::MessagePack: $Data::MessagePack::VERSION\n";
 print "Storable: $Storable::VERSION\n";
 cmpthese timethese(
     -1 => {
-        json     => sub { JSON::XS::encode_json($a) },
+        json     => sub { JSON::encode_json($a) },
         storable => sub { Storable::freeze($a) },
         mp       => sub { Data::MessagePack->pack($a) },
     }
