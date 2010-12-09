eaf9944e43778dc9c42a118d55003d7ccf0d2379
FURUHASHI Sadayuki <frsyuki@users.sourceforge.jp>
2010-12-09 17:31:29 +0900

    java: added ElementType.PARAMETER to the target of @Ignore, @Index, @Nulable, @Optional and @Required annotations for RPC

--- java/src/main/java/org/msgpack/annotation/Required.java
+++ java/src/main/java/org/msgpack/annotation/Required.java
@@ -22,7 +22,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.FIELD)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Required {
 }
