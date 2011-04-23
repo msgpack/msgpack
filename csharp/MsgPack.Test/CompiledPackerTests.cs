6cfea98501e3f56b0c8fbe29334d4a49b1fd57c3
Kazuki Oikawa <k@oikw.org>
2011-04-23 14:44:22 +0900

    csharp: add license, rename filename/namespace.

--- csharp/MsgPack.Test/CompiledPackerTests.cs
+++ csharp/MsgPack.Test/CompiledPackerTests.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using NUnit.Framework;
-using TestA_Class = msgpack.tests.ObjectPackerTests.TestA_Class;
-using TestB_Class = msgpack.tests.ObjectPackerTests.TestB_Class;
+﻿//
+// Copyright 2011 Kazuki Oikawa
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
-namespace msgpack.tests
+using NUnit.Framework;
+using TestA_Class = MsgPack.Test.ObjectPackerTests.TestA_Class;
+using TestB_Class = MsgPack.Test.ObjectPackerTests.TestB_Class;
+
+namespace MsgPack.Test
 {
 	[TestFixture]
 	public class CompiledPackerTests
