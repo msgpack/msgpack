748236fef0675b7fd97484018ca95fc887466a02
Muga Nishizawa <muga.nishizawa@gmail.com>
2012-02-09 02:21:43 +0900

    MessagePack for Java was moved to https://github.com/msgpack/msgpack-java.

--- java/src/main/java/org/msgpack/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * MessagePack is a binary-based efficient object serialization library.
- * It enables to exchange structured objects between many languages like JSON.
- * But unlike JSON, it is very fast and small.
- *
- * Use {@link Packer} to serialize and {@link Unpacker} to deserialize.
- */
-package org.msgpack;
