eae0532b8c9a6df499493ef9d82539a9acf627f1
INADA Naoki <inada-n@klab.com>
2012-08-20 22:04:44 +0900

    Split msgpack-php repository.

--- php/msgpack_class.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef MSGPACK_CLASS_H
-#define MSGPACK_CLASS_H
-
-#define MSGPACK_CLASS_OPT_PHPONLY -1001
-
-void msgpack_init_class();
-
-#endif
--- php/msgpack_convert.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef MSGPACK_CONVERT_H
-#define MSGPACK_CONVERT_H
-
-int msgpack_convert_object(zval *return_value, zval *object, zval **value);
-int msgpack_convert_array(zval *return_value, zval *tpl, zval **value);
-int msgpack_convert_template(zval *return_value, zval *tpl, zval **value);
-
-#endif
